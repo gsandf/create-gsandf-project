5d50f8d9feae15d4bdaf6d77baa9701c72a316ae
Blake Knight <bknight@gsandf.com>
2020-03-19 13:16:02 -0500

    :bug: fix: WordPress + Next.js example error page throws on build
    Resolves #9

--- examples/wordpress-next/packages/site/pages/_error.js
+++ examples/wordpress-next/packages/site/pages/_error.js
@@ -42,7 +42,7 @@ function Error({ statusCode }) {
 }
 
 Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  const statusCode = res?.statusCode ?? err?.statusCode ?? 404;
   return { statusCode };
 };
 
