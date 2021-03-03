71f78d3d76aaef4785f5a0dcb7a97de5262bcbf1
Blake Knight <bknight@gsandf.com>
2021-03-03 11:49:56 -0600

    remove more docker stuff; update API interfaces

--- examples/wordpress-next/packages/site/api.ts
+++ examples/wordpress-next/packages/site/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import LRUMap from 'quick-lru';
 import { absoluteURL } from './utils/absolute-url';
 
 const graphqlEndpoint = absoluteURL('/graphql');
+
+/** Time (in seconds) to cache content. Note, this is not in milliseconds. */
+export const defaultCacheTime = 600; // 10 minutes
 
 export const apiRequest = axios.create({
   baseURL: graphqlEndpoint,
@@ -14,50 +16,17 @@ export interface GraphQLQuery {
   variables?: any;
 }
 
-export interface GraphQLResponse {
-  data?: any;
-  errors?: any;
+export interface GraphQLResponse<Data = any> {
+  data: Data;
+  errors: any;
 }
 
-interface APIFetchArgs extends GraphQLQuery {
-  bypassCache?: boolean;
-  throwOnErrors?: boolean;
-}
-
-const cache = new LRUMap<string, GraphQLResponse>({ maxSize: 100 });
-
-export const clearGraphQLCache = () => cache.clear();
-
-export const getErrorMessage = (response: GraphQLResponse) =>
-  response?.errors[0]?.message;
-
-export const hasErrors = (response: GraphQLResponse) =>
-  response.errors !== undefined;
-
-export async function apiFetch({
-  bypassCache = false,
+export async function apiFetch<Data = any>({
   query,
-  throwOnErrors = true,
   variables
-}: APIFetchArgs): Promise<GraphQLResponse> {
-  const key = JSON.stringify([query, variables]);
-
-  // Fetch value if none stored in cache
-  if (bypassCache || !cache.has(key)) {
-    const value: GraphQLResponse = await apiRequest({
-      data: { query, variables },
-      method: 'POST'
-    }).then(response => response.data);
-
-    // If errors, return without caching
-    if (hasErrors(value)) {
-      if (throwOnErrors) throw new Error(getErrorMessage(value));
-      return value;
-    }
-
-    // Save value in cache
-    cache.set(key, value);
-  }
-
-  return cache.get(key) as GraphQLResponse;
+}: GraphQLQuery): Promise<GraphQLResponse<Data>> {
+  return apiRequest({
+    data: { query, variables },
+    method: 'POST'
+  }).then(response => response.data);
 }
