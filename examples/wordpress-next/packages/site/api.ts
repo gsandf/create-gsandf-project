81fdaea7e47c934547e027c5d7b7e354b022b4bd
Blake Knight <bknight@gsandf.com>
2021-05-21 11:34:41 -0500

    :rotating_light: throws on GraphQL error
    Previously, `data` would just be undefined if a GraphQL error occurred. This changes that so `apiFetch` throws. This means we can both get the error details if they exist and know data will be populated if there was no error.

--- examples/wordpress-next/packages/site/api.ts
+++ examples/wordpress-next/packages/site/api.ts
@@ -1,6 +1,29 @@
 import axios from 'axios';
 import { absoluteURL } from './lib/absolute-url';
 
+export interface GraphQLQuery {
+  query: string;
+  variables?: any;
+}
+
+interface GraphQLError {
+  message: string;
+  extensions?: any;
+  locations?: any;
+  nodes?: any;
+  originalError?: any;
+  path?: any;
+  positions?: any;
+  source?: any;
+}
+
+type GraphQLErrorResponse = { errors: GraphQLError[] };
+type GraphQLSuccessResponse<Data> = { data: Data };
+
+export type GraphQLResponse<Data = any> =
+  | GraphQLErrorResponse
+  | GraphQLSuccessResponse<Data>;
+
 const graphqlEndpoint = absoluteURL('/graphql');
 
 /** Time (in seconds) to cache content. Note, this is not in milliseconds. */
@@ -11,22 +34,28 @@ export const apiRequest = axios.create({
   headers: { Accept: 'application/json', 'Content-Type': 'application/json' }
 });
 
-export interface GraphQLQuery {
-  query: string;
-  variables?: any;
-}
+export const getErrorMessage = (response: GraphQLErrorResponse): string =>
+  response?.errors?.[0]?.message ?? 'An unknown error occured';
 
-export interface GraphQLResponse<Data = any> {
-  data: Data;
-  errors: any;
-}
+export const hasErrors = (
+  response: GraphQLResponse
+): response is GraphQLErrorResponse =>
+  'errors' in response && response.errors !== undefined;
 
 export async function apiFetch<Data = any>({
   query,
   variables
-}: GraphQLQuery): Promise<GraphQLResponse<Data>> {
-  return apiRequest({
-    data: { query, variables },
-    method: 'POST'
-  }).then(response => response.data);
+}: GraphQLQuery): Promise<GraphQLSuccessResponse<Data>> {
+  const response = await apiRequest
+    .request<GraphQLResponse<Data>>({
+      data: { query, variables },
+      method: 'POST'
+    })
+    .then(response => response.data);
+
+  if (hasErrors(response)) {
+    throw new Error(getErrorMessage(response));
+  }
+
+  return response;
 }
