fba851bde3700ddefd82c228ed805b9496831f39
Blake Knight <bknight@gsandf.com>
2021-06-30 12:56:29 -0500

    :sparkles: add `eslint-config-next`

--- examples/nextjs/.eslintrc.js
+++ examples/nextjs/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
-  extends: [require.resolve('amper-scripts/config/eslint')],
-  root: true
+  extends: [require.resolve('amper-scripts/config/eslint'), 'next'],
+  root: true,
+  rules: {
+    'import/no-anonymous-default-export': 'off'
+  }
 };
